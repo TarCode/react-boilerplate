26ce827bd915660099794454632c4ff8b3be1c40
TarCode <tarcode33@gmail.com>
2019-01-16 16:23:18 +0200

    connected home component to redux state

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { getData } from '../../actions/data' 
 
-export default () => (
-    <div>
-        <h1>Home</h1>
-    </div>
-)
+class Home extends Component {
+    componentDidMount() {
+        this.props.getData()
+    }
+
+    render() {
+        const { data, loading, err } = this.props
+        return (
+            <div>
+                <h1>Home</h1>
+                {
+                    loading ?
+                    <p>Loading...</p> :
+                    data.length > 0 ?
+                    data.map((item, index) => (
+                        <li key={index}>{item.name}</li>
+                    )) :
+                    <p>No items to show</p>
+                }
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    const { data, loading, err } = state.data
+    return {
+        data,
+        loading,
+        err
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        getData: bindActionCreators(getData, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
